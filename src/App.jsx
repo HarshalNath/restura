196e4a124ef642da6627a855db856e324a1e3ed7
Harshal Nath <harshalnath@protonmail.com>
2024-11-28 17:37:06 +0530

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dishes from "./components/Dishes";
 import HeroSection from "./components/HeroSection";
 import Mission from "./components/Mission";
 import Navbar from "./components/Navbar";
+import OurExpertise from "./components/OurExpertise";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Dishes />
       <About />
       <Mission />
+      <OurExpertise />
     </main>
   );
 }
